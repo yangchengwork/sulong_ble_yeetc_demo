8be16737392e3a4b1c27cd4c886df3164d2a2938
Gump Yang <gump.yang@sulongcom.com>
2016-11-30 19:10:46 +0800

    初步可以反复通讯

--- cordova_src/www/js/index.js
+++ cordova_src/www/js/index.js
@@ -16,7 +16,16 @@
  * specific language governing permissions and limitations
  * under the License.
  */
+
+let SULONG_SERVICE_UUID = "cc747268-008d-48c1-9541-e428c310b400";
+let SULONG_WRITE_UUID = "cc747268-008d-48c1-9541-e428c310b401";
+let SULONG_NOTIFY_UUID = "cc747268-008d-48c1-9541-e428c310b402";
+
 var app = {
+    step: 0,
+    count: 0,
+    deviceid: 0,
+    time: null,
     // Application Constructor
     initialize: function() {
         document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
@@ -30,16 +39,86 @@ var app = {
         this.receivedEvent('deviceready');
     },
 
+    buttonHandle: function() {
+        var message = "info";
+        switch (app.step) {
+            case 0:
+            message = "scan";
+            ble.scan([SULONG_SERVICE_UUID], 5, app.onScan, app.onScanFail);
+            break;
+            case 1:
+            app.time = setInterval(app.onTimer, 1000);
+            break;
+        }
+        $('#info').text(message);
+    },
+
+    onTimer: function() {
+        app.sendCmd(0x31);
+    },
+
+    sendCmd: function(cmd) {
+        var data = new Uint8Array(4);
+        data[0] = 0x74;
+        data[1] = 0x70;
+        data[2] = 0x3D;
+        data[3] = cmd;
+        ble.write(app.deviceid, SULONG_SERVICE_UUID, SULONG_WRITE_UUID, data.buffer, function() {
+            console.log("write success");
+        }, app.onError);
+    },
+
+    onScan: function(device) {
+        console.log("scan device " + JSON.stringify(device));
+        app.deviceid = device.id;
+        ble.stopScan(app.onStopScan, app.onError);
+        app.connect(device);
+    },
+
+    connect: function(device) {
+        var onConnect = function() {
+            ble.startNotification(app.deviceid, SULONG_SERVICE_UUID, SULONG_NOTIFY_UUID, app.onData, app.onError);
+            $('#btn').text("start");
+            app.step = 1;
+        };
+        ble.connect(app.deviceid, onConnect, app.onError);
+    },
+
+    onData: function(buffer) {
+        var data = new Uint8Array(buffer);
+        var len = data.length;
+        if ((data[len-2] == 0x0d) && (data[len-1] == 0x0a)) {
+            if (data[3] == 0x31) {
+                app.sendCmd(0x34);
+            }
+        }
+    },
+
+    onStopScan: function() {
+        console.log("stop scan device");
+    },
+
+    onScanFail: function(reason) {
+        app.onDataInit();
+        app.onError(reason);
+    },
+
+    onDataInit: function() {
+        app.step = 0;
+        app.count = 0;
+    },
+
+    onError: function(reason) {
+        console.log("ERROR: " + JSON.stringify(reason));
+    },
+
     // Update DOM on a Received Event
     receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
-
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
-
         console.log('Received Event: ' + id);
+        app.onDataInit();
+        $('#btn').on('click', function () {
+            app.buttonHandle();
+        });
     }
 };
 
